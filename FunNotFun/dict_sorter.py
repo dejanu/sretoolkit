7261bb0b503eb3b06e8597d3f2d0dd34211e0cc0
Dej <dejanualex@yahoo.com>
2019-06-07 15:03:13 +0300

    les dict

---
+++ FunNotFun/dict_sorter.py
@@ -0,0 +1,42 @@
+# given dict
+
+d = {"f1":312,"f2":432,"f3":312, "f4":432}
+
+# create
+
+dd = {"f1":312,"f2":432}
+
+def unqiue_values_dict(d):
+    dd = dict()
+
+def sort_keys(d):
+    """sorting by key"""
+    dd = dict()
+    key_sort = sorted(d.keys())
+    for i in key_sort:
+        dd[i] = d[i]
+    return dd
+
+def sort_values(d): 
+
+##    get_val(d):
+##        return d[1]
+    
+    sorted_d = sorted(d.items(), key = get_val)
+      return sorted_d
+
+if __name__ == "__main__":
+
+    test_dict = {'c': 3, 'b':45, 'd':32, 'a': 1}
+
+    #sortare directa
+    #print({i:test_dict[i] for i in sorted(test_dict.keys())})
+    
+    #print(sort_dict(d))
+    #print(sort_values(test_dict))
+
+    def dict_val(test_dict):
+        return test_dict[1]
+   
+    sorted_x = sorted(test_dict.items(), key=dict_val)
+    print(sorted_x)
---
+++ FunNotFun/indexequilibrium.py
@@ -0,0 +1,48 @@
+def testequilibrium(arr):
+    """
+    """
+    
+    n = len(arr) 
+  
+    # Check for indexes one by one  
+    # until an equilibrium index is found 
+    for i in range(n): 
+        leftsum = 0
+        rightsum = 0
+      
+        # get left sum 
+        for j in range(i): 
+            leftsum += arr[j] 
+          
+        # get right sum 
+        for j in range(i + 1, n): 
+            rightsum += arr[j] 
+          
+        # if leftsum and rightsum are same, 
+        # then we are done 
+        if leftsum == rightsum: 
+            return i 
+      
+    # return -1 if no equilibrium index is found 
+    return -1
+
+
+def myequi(l):
+    for i in range(len(l)):
+        ss = 0
+        sd = 0
+        for j in range(i):
+            ss+=l[j]
+        for j in range(i+1,len(l)):
+            sd+=l[j]
+        if ss == sd:
+            return i
+    return -1
+
+if __name__ == "__main__":
+
+    a = myequi([-7, 1, -3, 7, -4, -7, 0])
+    b = myequi([-7, 1, 5, 2, -4, 3, 0])
+    print(a)
+    print(b)
+    
