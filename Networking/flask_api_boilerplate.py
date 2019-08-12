75515b1fa832c4b59d5efc16846a2c9de5c71175
dej <alexandru.dejanu@orange.com>
2019-08-12 10:48:15 +0300

    add GET and read http params

--- Networking/flask_api_boilerplate.py
+++ Networking/flask_api_boilerplate.py
@@ -1,4 +1,4 @@
-from flask import request,jsonify, Flask
+from flask import request,jsonify, Flask, make_response
 
 
 app = Flask(__name__)
@@ -8,6 +8,10 @@ app = Flask(__name__)
 
 ## POST to /data
 # curl -vX POST -d @input.json http://alex:admin@127.0.0.1:5000/upload --header "Content-Type: application/json"
+
+## GET request /raport?currency=USD
+# curl  -X GET http://alex:admin@127.0.0.1:5000/raport?currency=USD
+# curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://alex:admin@127.0.0.1:5000/raport?currency=USD
 
 @app.route("/", methods = ["GET"])
 def main():
@@ -21,8 +25,20 @@ def main():
 def data():
     """ get the json from body of POST request"""
     r = request.json
+    #ignore content-type
+    #r = request.get_json(force=True)
     print(r)
     return jsonify(r)
+
+@app.route("/raport", methods=["GET"])
+def get_data():
+    """ get raport """
+    # dict object request.args
+    print(request.args)
+    return request.args
+    
+    
+
 
 
 if __name__ == "__main__":
