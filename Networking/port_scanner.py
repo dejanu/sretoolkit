25cd90b16a3b69c3d99c990d9e998153db78955c
Dej <dejanualex@yahoo.com>
2020-01-29 09:46:59 +0200

    Update port_scanner.py

--- Networking/port_scanner.py
+++ Networking/port_scanner.py
@@ -13,7 +13,7 @@ last_port = int(portrange.split('-')[-1])
 print("Scanning host {0}, from port {1} to port {2}".format(target,first_port,last_port))
 
 
-from p in range(first_port, last_port):
+for p in range(first_port, last_port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     status = s.connect_ex((target,port))
     if (status == 0):
