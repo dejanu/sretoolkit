538fb44c33ae2e0c8a4e69c8b449c4e00a366be3
Dej <dejanualexandru@gmail.com>
2021-07-07 16:51:29 +0300

    old school verification

--- OOP/check_obj_attr.py
+++ OOP/check_obj_attr.py
@@ -1,11 +1,21 @@
-# user.py
 
-### uses functionality provided by library.py
+ ### library.py - provides classes for business
+
+class Base:
+
+	# abstract method:	
+	def implement_me(self):
+		raise NotImplementedError("To be implemented")
+
+	def foo(self):
+		return 'foo'
+############################################################################### diffrent files
+
+# user.py - uses functionality provided by library.py
 
 from library import Base
 
 ## verify if methods exists in the module which was imported
-
 assert hasattr(Base, 'foo'), "there is no foo method in Base class"
 
 class Derived(Base):
