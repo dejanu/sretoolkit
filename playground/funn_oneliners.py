a0cf4353625d85f5b3faa5f7f57a9b940f672fe7
dejanualex <dejanualexandru@gmail.com>
2021-08-13 11:09:36 +0300

    Update funn_oneliners.py

--- playground/funn_oneliners.py
+++ playground/funn_oneliners.py
@@ -4,6 +4,7 @@
 # if ((bprm->buf[0] != '#') || (bprm->buf[1] != '!')) reads the very first bytes of the file, and compares them to #!
 # if comparison is True the Linux kernel, which makes another exec call with path /usr/bin/env python and current file as the first argument
 
+# oneliner oneliners
 
 #fibonacci
 fib = lambda n: n if n < 2 else (fib(n - 1) + fib(n - 2))
@@ -11,3 +12,8 @@ fib = lambda n: n if n < 2 else (fib(n - 1) + fib(n - 2))
 #gotcha get ip
 import socket
 socket.gethostbyname(socket.gethostname())
+
+
+## Start a simple webserver running in the background on port 8000 with the current directory as the doc root
+## sending stdout and stderr to /dev/null. Also making sure it continues running when the shell is closed.
+nohup python3 -m http.server > /dev/null 2>&1 & 
