4d8462e979bc556633f3da5fac8e1fba93ca73bf
dejanualex <dejanualexandru@gmail.com>
2021-08-13 11:12:19 +0300

    delete quine

--- playground/quine.py
+++
@@ -1,5 +0,0 @@
-#quine
-#%s prints the str() of an object (What you see when you print(object)).
-#%r prints the repr() of an object (What you see when you print(repr(object))
-s='s=%r;print(s%%s)';print(s%s)
-
