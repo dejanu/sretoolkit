1413f7662e80f74296d4ad8893e0c01da66297b9
dejanualex <dejanualexandru@gmail.com>
2021-08-16 14:36:20 +0300

    Update funn_oneliners.py

--- playground/funn_oneliners.py
+++ playground/funn_oneliners.py
@@ -16,7 +16,9 @@ socket.gethostbyname(socket.gethostname())
 
 ## Start a simple webserver running in the background on port 8000 with the current directory as the doc root
 ## sending stdout and stderr to /dev/null. Also making sure it continues running when the shell is closed.
-nohup python3 -m http.server > /dev/null 2>&1 & 
+nohup python3 -m http.server 80 > /dev/null 2>&1 & 
+# ptyhon 2
+python -m SimpleHTTPServer 80
 
 
 #quine - no input and produces a copy of its own source
