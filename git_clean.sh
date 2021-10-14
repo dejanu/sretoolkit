285f4825472e777d4d455d4724471109c8e42066
dejanualex <dejanualexandru@gmail.com>
2021-10-14 18:24:00 +0300

    Update git_clean.sh

--- git_clean.sh
+++ git_clean.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
-echo -e "Checking remote...\nListing MERGED branches:"
+# some local cleanup:delete refs not local branches
+git fetch -p
+#display all remotes associated with origin git remote show origin
+echo -e "-------> Checking remote...\nListing MERGED branches:"
 git remote -v && git branch -r --merged | grep -v HEAD | xargs -L1 git --no-pager log --pretty=tformat:'%Cgreen%d%Creset | %h | %an | %Cblue%ar%Creset' -1 | column -t -s '|'
 if [ $? -eq 0 ]; then
-	read -p 'Delete merged branches: yes/no ' anw
+        read -p 'Delete REMOTE MERGED branches: yes/no ' anw
 fi
 if [ $anw == "yes" ];then
-	#git branch -r --merged master | grep -vE "develop|master|release" | awk -F'/' '{print $2}'|xargs -n 1 git push --delete origin
-	git branch -r --merged master | grep -vE "develop|master|release"
+        git branch -r --merged master | grep -vE "develop|master|release" | awk -F'/' '{print $2}'|xargs -n 1 git push --delete origin
 else
-	echo Nothing to do
+        echo Nothing to do
 fi
