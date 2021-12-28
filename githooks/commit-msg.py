ad6c3994311b3cb15b3aaee82e07c7a44ed52161
deal <alexandru.dejanu@systematic.com>
2021-12-28 17:54:56 +0200

    some fun git hooks

---
+++ githooks/commit-msg.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+import re
+
+def line_valid(line_no, line):
+    """
+    return True if line is ok
+    """
+    if line_no == 0:
+        # first line should have the task idetifier e.g TASK-4351
+        return re.match("^TASK-\d{1,5}.*", line)
+ 
+def show_rules():
+    """
+    Rules for a great git commit message style
+    """
+    print("Please follow the rules add task/feature COPS-xxxx in the name of the commit msg""")
+
+def main():
+    print("Args are {}".format(sys.argv))
+    # read commit-msg
+    with open(sys.argv[1], "r") as fp:
+        lines = fp.readlines()
+        for l in lines:
+            print(l)
+        for line_no, line in enumerate(lines):
+            #print(line_no,line)
+            if not line_valid(line_no, line):
+                show_rules()
+                sys.exit(1)
+    sys.exit(0)
+
+if __name__=="__main__":
+    main()
--- githooks/post-checkout.sh
+++ githooks/post-checkout.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/bash
 
 # stats about merged and unmerged branches
@@ -12,10 +13,10 @@ git fetch -p
 echo -e "-------> Checking remote...\nListing MERGED branches:"
 git remote -v && git branch -r --merged | grep -v HEAD | xargs -L1 git --no-pager log --pretty=tformat:'%Cgreen%d%Creset | %h | %an | %Cblue%ar%Creset' -1 | column -t -s '|'
 if [ $? -eq 0 ]; then
-        read -p 'Delete REMOTE MERGED branches: yes/no ' anw
+	read -p 'Delete REMOTE MERGED branches: yes/no ' anw
 fi
 if [ $anw == "yes" ];then
-        git branch -r --merged master | grep -vE "develop|master|release" | awk -F'/' '{print $2}'|xargs -n 1 git push --delete origin
+	git branch -r --merged master | grep -vE "develop|master|release" | awk -F'/' '{print $2}'|xargs -n 1 git push --delete origin
 else
-        echo Nothing to do
+	echo Nothing to do
 fi
