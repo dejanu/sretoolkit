e9f9b0838a8a72a0725e70b7b8fab6629a328e89
dejanualex <dejanualex@yahoo.com>
2022-04-13 18:42:45 +0300

    Update cluster_state.sh

--- cluster_state.sh
+++ cluster_state.sh
@@ -31,14 +31,35 @@ echo "Start debug pod for a node: oc debug node/<NODE_NAME>"
 
 #==================================================================ALIASES
 
-# list all pods/deployments/services/persistent volumes from all namespaces
-alias kc='f(){ kubectl get $1 -A -o go-template="{{range .items}} --> {{.metadata.name}} in namespace: {{.metadata.namespace}}{{\"\n\"}}{{end}}"; unset -f f; }; f'
+# check health Metrics API
+alias kstate='kubectl get --raw "/healthz?verbose"'
+
+# list all obj type from all namespaces, usage: ko po/svc/deployment/pv
+alias ko='f(){ kubectl get $1 -A -o go-template="{{range .items}} --> {{.metadata.name}} in namespace: {{.metadata.namespace}}{{\"\n\"}}{{end}}"; unset -f f; }; f'
+
+# check no of pods per node
+alias knode_pods='kubectl get po -ojson -A | jq ".items | group_by(.spec.nodeName) | map({"nodeName": .[0].spec.nodeName, "count": length}) | sort_by(.count)"'
 
 # check pods which are running or not
 alias pods_notrunning='kubectl get pods -A --field-selector=status.phase!=Running'
 alias pods_running='kubectl get pods -A --field-selector=status.phase=Running'
 
-# check namespace events
-alias namespace_events='for n in $(kubectl get ns -o go-template="{{range .items}}{{.metadata.name}}{{\"\n\"}}{{end}}");do kubectl get events -n $n;done'
+# check restarted pods 
+alias pods_restarted='kubectl get pods -A --sort-by=.status.containerStatuses[0].restartCount!=0'
 
-# check vault status
+# check namespace events
+alias kns_events='for n in $(kubectl get ns -o go-template="{{range .items}}{{.metadata.name}}{{\"\n\"}}{{end}}");do kubectl get events -n $n;done'
+
+#############
+
+# get pod just name
+kubectl get pods -A -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
+
+# get pod pod/name
+kubectl get po -oname -A
+
+# get custom columns e.g. NAME columns
+kubectl get po -o=custom-columns=NAME:.metadata.name -A
+
+# get pod name with go templates are a powerful method to customize output however you want
+kubectl get po -A -o go-template='{{range .items}} --> {{.metadata.name}} in namespace: {{.metadata.namespace}}{{"\n"}}{{end}}'
