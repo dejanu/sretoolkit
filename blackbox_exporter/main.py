33c291b9dcc1da4c5fb821d7f15d95bb5e2ad376
dejanualex <dejanualexandru@gmail.com>
2023-08-10 18:13:44 +0300

    wowowow

--- blackbox_exporter/main.py
+++ blackbox_exporter/main.py
@@ -77,7 +77,6 @@ app.config["DEBUG"] = True
 @app.route('/metrics')
 def metrics():
     """ Return promethues metrics """
-    dev = myMetrics("dev.phi.projects.systematic-synergy.io")
     return generate_latest()
 
 @app.errorhandler(500)
@@ -98,7 +97,9 @@ if __name__ == "__main__":
     g = Gauge('synergy_components', 'Gauge metric for Synergy main components',['cluster','vault','registry','console'])
     # if at least one component is unhealthy, the gauge will be 0
     # g.labels(....).set(0)
+
     g.labels(cluster=dev.cluster_base_domain, vault=dev.get_vault_status(), registry=dev.get_registry_status(), console=dev.get_console_status()).set_function(lambda: 0 if dev.get_vault_status() != "healthy" else 1)
+    g.labels(cluster="mamita", vault=dev.get_vault_status(), registry=dev.get_registry_status(), console=dev.get_console_status()).set_function(lambda: 0 if dev.get_vault_status() != "healthy" else 1)
     
     
     app.run(host='0.0.0.0', port=8080)
