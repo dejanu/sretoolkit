686960268298f2711186d4f0f1e627049aa74f28
dejanu <dejanualexandru@gmail.com>
2024-09-18 16:10:20 +0300

    forgot about this

--- docker_stuff/docker_image_scanner.sh
+++ docker_stuff/docker_image_scanner.sh
@@ -1,36 +1,44 @@
 
 #!/usr/bin/env bash
 
+# CreatedSince: how long vs. CreatedAt: exact timestamp
+echo "You have $(docker system info --format '{{ .Images}}') images on your machine:"
+docker images --format "{{ .Repository }}:{{ .Tag }} -----> Created: {{ .CreatedSince }}"
+
+echo "Pass the image name to scan: "
+read image_name
+
+echo -e "\e[33mScanning image...${image_name}, will be generated at /tmp/report_${date +%d_%m_%Y_%S}.html\e[0m"
+
+# trivy scan
+mkdir -p /tmp/report
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/Library/Caches:/root/.cache/ -v /tmp/report:/tmp aquasec/trivy:latest image \
+-f template --template "@contrib/html.tpl" -o /tmp/report_${image_name}.html ${image_name}
 
 
-# create function called scan_image which takes one argument: the image name
-scan_image() {
-    # trivy scan
-    docker run --rm -v trivy-cache:/root/.cache/ -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:latest image ${image}
-    # docker run --rm \
-        # --volume /var/run/docker.sock:/var/run/docker.sock \
-        # --name GrypeScanner anchore/grype:latest \
-        # ${i}
+# scan_image() {
+#     # trivy scan
+#     docker run --rm -v trivy-cache:/root/.cache/ -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:latest image ${image}
+#     # docker run --rm \
+#         # --volume /var/run/docker.sock:/var/run/docker.sock \
+#         # --name GrypeScanner anchore/grype:latest \
+#         # ${i}
     
-    #
-}
+#     #
+# }
 
-# echo to stdout images on host
-images=$(docker system info --format '{{ .Images}}')
-echo "You have ${images} images on your machine:"
-docker images
 
-# loop through images and interactively ask the user if they want to scan the image
-for image in $(docker images --format "{{.Repository}}:{{.Tag}}")
-    do
-        echo "Do you want to image ${image} ? (y/n)"
-        read answer
-        if [ "$answer" == "y" ]
-        then
-            echo "Scanning image: ${image}"
-            scan_image ${image}
-        else
-            echo "Skipping image: ${image}"
-        fi
-    done
+# # loop through images and interactively ask the user if they want to scan the image
+# for image in $(docker images --format "{{.Repository}}:{{.Tag}}")
+#     do
+#         echo "Do you want to image ${image} ? (y/n)"
+#         read answer
+#         if [ "$answer" == "y" ]
+#         then
+#             echo "Scanning image: ${image}"
+#             scan_image ${image}
+#         else
+#             echo "Skipping image: ${image}"
+#         fi
+#     done
 
