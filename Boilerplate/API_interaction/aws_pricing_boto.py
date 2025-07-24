f1beb60c44df80c308e53bcd581321cc7c30bd7c
dejanu <dejanualexandru@gmail.com>
2025-07-24 17:18:37 +0300

    sum boto stuff
    Signed-off-by: dejanu <dejanualexandru@gmail.com>
    
    sum boto stuff
    
    Signed-off-by: dejanu <dejanualexandru@gmail.com>

---
+++ Boilerplate/API_interaction/aws_pricing_boto.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# AWS SDK for Python (Boto3)
+import boto3
+import os
+import json
+
+boto3_client = boto3.client('pricing',aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), 
+                      aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"), 
+                      region_name="us-east-1")
+
+
+response = boto3_client.get_products(ServiceCode="AmazonRDS", Filters = [
+                                        {
+                                        'Field': 'volumeName',
+                                        'Type': 'CONTAINS',
+                                        'Value': 'io1',
+                                    },
+                                  ],
+                                  FormatVersion='aws_v1', 
+                                  MaxResults=100)
+
+price_list = response.get("PriceList", [])
+for item in price_list:
+    parsed = json.loads(item)
+    print(json.dumps(parsed, indent=2))
---
+++ Boilerplate/API_interaction/aws_s3_boto.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import boto3
+import uuid
+from botocore.exceptions import ClientError
+
+## https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html#using-environment-variables
+
+## boto3.client() = low level interace that maps directly to AWS service APIs and returns raw dict from AWS API
+## botot3.resource() = high level OOP interfaces
+
+
+# s3_client = boto3.client('s3',aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), 
+#                       aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"), 
+#                       region_name="us-east-1")
+
+# response = s3_client.list_buckets() # Raw dict response
+# buckets =  response["Buckets"]
+# print(buckets)
+
+
+# s3 = boto3.resource('s3') 
+# for bucket in s3.buckets.all():  # Returns bucket objects
+#     print(bucket.name)
+
+##############################
+## create bucket
+## push a file into a bucket
+## delete bucket
+
+# create client
+s3 = boto3.resource('s3')
+
+def create_bucket(client, bucket_name):
+    # generate unique uuid
+    suffix = str(uuid.uuid4()).split("-")[0]
+    return client.create_bucket(Bucket = f"{bucket_name}-{suffix}")
+
+def list_buckets(client):
+    '''https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/service-resource/buckets.html'''
+    for b in client.buckets.all(): # return an interator object with all buckets 
+        print(b)
+
+def delete_bucket(client,bucket_name):
+    '''https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/bucket/delete.html'''
+    bucket = client.Bucket(bucket_name)
+    
+    try:
+        bucket.delete()
+    except ClientError as err:
+        print(f"Bucket name does not exist:{err.response['Error']}")
+        return 
+
+def upload_file(client,bucket_name,file):
+    
+
+if __name__ == "__main__":
+
+    # list_buckets(s3)
+    # create_bucket(s3, bucket_name = "titi")
+    list_buckets(s3)
+    delete_bucket(s3,'tto-bbb5c0ca')
+    list_buckets(s3)
