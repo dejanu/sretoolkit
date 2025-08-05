f9ce34d3a24d0a1c95ce5f8fabb0575687b6a500
dejanualex <dejanualexandru@gmail.com>
2025-08-05 20:17:34 +0300

    clean
    Signed-off-by: dejanualex <dejanualexandru@gmail.com>

--- serialization_context_manager_streams/csv_creation.py
+++ serialization_context_manager_streams/csv_creation.py
@@ -1,34 +1,27 @@
+#!/usr/bin/env python3
+
 import csv
 
-#create a CSV file
-'''
-nume=["nume","prenume","id"]
-lista=[["dejanu","alex",1],["roby","roberto",2]]
+def print_csv(filename):
+    with open(filename,'r') as f:
+        line_count = 0
+        reader = csv.reader(f, delimiter=',', quotechar='"')
+        # column is line_count == 0
+        for row in reader:
+            if line_count == 0:
+                print(f"Column are {' '.join(row)}")
+                line_count += 1
+            else:
+                if row:  # Check if row is not empty
+                    line_count += 1
+                    print(row)
+        print(f"Processed {line_count} lines.")
 
-
-f=open("date.csv","wb")
-objCSV=csv.writer(f,delimiter=',',quotechar="'", quoting=csv.QUOTE_ALL)
-
-objCSV.writerow(nume)
-objCSV.writerows(lista)
-
-f.close()
-'''
-
-#read a CSV file
-f=open('pin.csv','rb')
-#reader=csv.reader(f,delimiter=",",quotechar="'",quoting=True)
-reader=csv.reader(f,delimiter=",",quoting=True)
-lista=list(reader)
-print(lista)
-f.close()
-
-
-#convert from text to csv
-with open(sys.argv[1], 'r') as in_file:
-    stripped = (line.strip() for line in in_file)
-    lines = (line.split(",") for line in stripped if line)
-    with open('renameit.csv', 'w') as out_file:
-        writer = csv.writer(out_file)
-        writer.writerow(('title', 'intro'))
-        writer.writerows(lines)
+if __name__ == "__main__":
+    # read using csv dict reader (columns are the keys)
+    d = {}
+    with open('rsvp.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            d[row.get('name')] = {k:v for k,v in row.items() if k != 'name'}
+    print(d)
